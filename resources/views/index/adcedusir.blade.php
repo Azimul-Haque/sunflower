e96ab291b7d13daf6792e0acb34aa2e9b78596ef
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 02:45:47 +0600

    Commit Mon 01/13/2025  2:45:47.28 30700

--- resources/views/index/adcedusir.blade.php
+++ resources/views/index/adcedusir.blade.php
@@ -52,7 +52,7 @@
                 <div class="col-md-8">
                     <div class="detail-content">
                         <h2>FARJANA ALAM</h2>
-                        <p class="title">Additional Deputy Commissioner (Edu & ICT)</p>
+                        <p class="title">Additional Deputy Commissioner (Edu & ICT), Narsingdi</p>
                         <p>As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality.</p>
 
                         <p>As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality.</p>
--- resources/views/index/dcnarsingdisir.blade.php
+++ resources/views/index/dcnarsingdisir.blade.php
@@ -52,7 +52,7 @@
                 <div class="col-md-8">
                     <div class="detail-content">
                         <h2>MOHAMMAD RASHED HOSSAIN CHOUDHURY</h2>
-                        <p class="title">Deputy Commissioner & District Magistrate</p>
+                        <p class="title">Deputy Commissioner & District Magistrate, Narsingdi</p>
                         <p>As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality.</p>
 
                         <p>As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality.</p>
