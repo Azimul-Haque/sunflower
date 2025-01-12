15d61c7bcf4d4c87ce4698968732cebfab9daa5e
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 02:10:52 +0600

    Commit Mon 01/13/2025  2:10:52.30 15357

--- resources/views/index/history.blade.php
+++ resources/views/index/history.blade.php
@@ -52,7 +52,7 @@
             <div class="row">
                 <div class="col-lg-6 col-12">
                     <div class="about-text">
-                        <h2>ইতিহাস ২০০০+ বা ২০০০-</h2>
+                        <h2>History forwards</h2>
                         <p>As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality.</p>
                         <p>face physical experiences, and bringing their essence to life, not only through digital immersion creativity but through new technology as well including Virtual Reality As marketing professionals in the experiential world, we have become accusto to the idea of an ever evolving industry. Brands today are moving away from purely face to face physical experiences,</p>
                     </div>
