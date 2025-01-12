1bf54c44bfcc327a885dc0581d27e57c4a86771a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 03:29:33 +0600

    Commit Mon 01/13/2025  3:29:33.23 2699

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -269,7 +269,7 @@
     				<div class="single-fact">
     					<i class="fa fa-trophy"></i>
     					<div class="number"><span class="counter">300</span>+</div>
-    					<p>10+ Awards Won</p>
+    					<p>Awards Won</p>
     				</div>
     				<!--/ End Single Fact -->
     			</div>
