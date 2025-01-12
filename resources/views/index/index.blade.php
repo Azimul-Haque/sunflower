cef1fb0d60c8a3e1f9e258c3d6298dbed049c6c3
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 03:21:12 +0600

    Commit Mon 01/13/2025  3:21:12.21 5524

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -67,7 +67,7 @@
     			<div class="col-lg-5 offset-lg-6 col-12">
     				<div class="cta-inner overlay">
     					<div class="text-content">
-    						<h2>We Focus On Brands, Products & Campaigns</h2>
+    						<h2>We Focus On Excellence in Education and Character Development</h2>
     						<p>Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore cum. Coluta nobis est eligendi optio cumque nihil impedit quo minusd quod maxime placeat facere possimus, omnis voluptas assumenda est. Our friendly support team is standing by to help you every step of the way.Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore cum. Coluta nobis est eligendi optio cumque nihil impedit quo minusd quod maxime placeat facere possimus, omnis voluptas assumenda est. Our friendly support team is standing by to help you every step of the way.</p>
     						<div class="button">
     							<a class="btn primary wow fadeInUp" href="contact.html" >Register Now</a>
