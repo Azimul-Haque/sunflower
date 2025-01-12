0bfb2b3d40e848e855c94edea2ae56d6812cb82a
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 01:24:08 +0600

    Commit Mon 01/13/2025  1:24:08.29 15460

--- resources/views/dashboard/abouts.blade.php
+++ resources/views/dashboard/abouts.blade.php
@@ -31,7 +31,7 @@
               </div>
               <div class="input-group form-group">
                 <span class="input-group-addon"><i class="fa fa-envelope"></i></span>
-                {!! Form::text('shorttext', null, array('class' => 'form-control text-blue', 'required' => '', 'placeholder' => 'Write Email Address')) !!}
+                {!! Form::text('shorttext', null, array('class' => 'form-control text-blue', 'required' => '', 'placeholder' => 'Write Short Text')) !!}
               </div>
               <div class="input-group form-group">
                 <span class="input-group-addon"><i class="fa fa-facebook-official"></i></span>
