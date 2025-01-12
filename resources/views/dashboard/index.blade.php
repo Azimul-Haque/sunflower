59ffc5b381013c0fdf4a8b1fc012c7534b73b7b9
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-13 02:50:32 +0600

    Commit Mon 01/13/2025  2:50:32.21 8995

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,6 +1,6 @@
 @extends('adminlte::page')
 
-@section('title', 'ড্যাশবোর্ড | সানফ্লাওয়ার কেজি অ্যান্ড প্রি-ক্যাডেট স্কুল')
+@section('title', 'Dashboard | ')
 
 @section('content_header')
     <h1><i class="fa fa-pic"></i> ড্যাশবোর্ড</h1>
