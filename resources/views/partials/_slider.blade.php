9e07daa56dcaa00a7dafb25f9577cbfef6f1f496
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-17 02:30:48 +0600

    Commit Fri 01/17/2025  2:30:48.23 99

--- resources/views/partials/_slider.blade.php
+++ resources/views/partials/_slider.blade.php
@@ -1,24 +1,26 @@
 <!-- Slider Area -->
 <section class="home-slider">
     <div class="slider-active">
-        <!-- Single Slider -->
-        <div class="single-slider overlay" style="background-image:url('/images/slider/1.jpg')">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-8 col-md-8 col-12">
-                        <div class="slider-text">
-                            <h1>Perfect Template for <span>Education</span> & Courses Website</h1>
-                            <p>Our mission is to empower clients, colleagues, and communities to achieve aspirations while building lasting, caring relationships.</p>
-                            <div class="button">
-                                <a href="courses.html" class="btn primary">Our Courses</a>
-                                <a href="about.html" class="btn">About Test</a>
+        @foreach()
+            <!-- Single Slider -->
+            <div class="single-slider overlay" style="background-image:url('/images/slider/1.jpg')">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-lg-8 col-md-8 col-12">
+                            <div class="slider-text">
+                                <h1>Perfect Template for <span>Education</span> & Courses Website</h1>
+                                <p>Our mission is to empower clients, colleagues, and communities to achieve aspirations while building lasting, caring relationships.</p>
+                                <div class="button">
+                                    <a href="courses.html" class="btn primary">Our Courses</a>
+                                    <a href="about.html" class="btn">About Test</a>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <!--/ End Single Slider -->
+            <!--/ End Single Slider -->
+        @endforeach
         
 
 
