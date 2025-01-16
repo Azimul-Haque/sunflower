94016642c13710c463424bda5cb937bb862a91a5
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-17 03:05:31 +0600

    Commit Fri 01/17/2025  3:05:31.23 30235

--- resources/views/dashboard/testimonials.blade.php
+++ resources/views/dashboard/testimonials.blade.php
@@ -36,7 +36,7 @@
                 </div>
                 <div class="form-group">
                   <label>প্রশংসাবাক্য</label>
-                  {!! Form::textarea('message', null, array('class' => 'form-control text-green', 'placeholder' => 'সাব-টাইটেল', 'required' => '', 'data-parsley-required-message' => 'পদবি আবশ্যক')) !!}
+                  {!! Form::textarea('message', null, array('class' => 'form-control text-green', 'placeholder' => 'সাব-টাইটেল', 'required' => '', 'data-parsley-required-message' => 'প্রশংসাবাক্য আবশ্যক')) !!}
                 </div>
 
 
