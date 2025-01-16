94ba87fad9334629ab1147f44a652aff32a25bd6
Numaira-Home-PC <orbachinujbuk@gmail.com>
2025-01-17 02:31:09 +0600

    Commit Fri 01/17/2025  2:31:09.26 14038

--- resources/views/partials/_slider.blade.php
+++ resources/views/partials/_slider.blade.php
@@ -20,7 +20,29 @@
                 </div>
             </div>
             <!--/ End Single Slider -->
-        @endforeach        
+        @endforeach
+        
+
+
+        <!-- Single Slider -->
+        <div class="single-slider overlay" style="background-image:url('/images/slider/2.jpg')">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 offset-lg-2 col-md-8 offset-md-2 col-12">
+                        <div class="slider-text text-center">
+                            <h1>Creative Template for <span>Education</span> & Courses Website</h1>
+                            <p>Our mission is to empower clients, colleagues, and communities to achieve aspirations while building lasting, caring relationships.</p>
+                            <div class="button">
+                                <a href="courses.html" class="btn primary">Our Courses</a>
+                                <a href="about.html" class="btn">About Test</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--/ End Single Slider -->
+        
     </div>
 </section>
 <!--/ End Slider Area -->
